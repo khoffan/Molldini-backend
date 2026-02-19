7b239dd585d63ba92c4db90e62b546601e04eb9e
khoffan <khoffan222@gmail.com>
2026-02-20 01:02:01 +0700

    chore: update prisma conf file

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,8 @@
 // npm install --save-dev prisma dotenv
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
+import dotenv from "dotenv";
+dotenv.config();
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
